9ebe49803d5531cea88b56f391c78be535cad2d5
Michael Bernstein <michael.bernstein@clicksign.com>
2015-07-13 13:49:29 -0300

    fix typo

--- db/migrate/20150624221355_add_companyid_to_profiles.rb
+++ db/migrate/20150624221355_add_companyid_to_profiles.rb
@@ -1,5 +1,5 @@
 class AddCompanyidToProfiles < ActiveRecord::Migration
   def change
-    add_column :profiles, :linkedin_company_id, :interger
+    add_column :profiles, :linkedin_company_id, :integer
   end
 end
