7bee0fe7218b0c0e76ddb14a071bbcd7764493f8
Michael Bernstein <michael.bernstein@clicksign.com>
2015-10-04 14:14:35 -0300

    Destroy profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -61,7 +61,7 @@ class ProfilesController < ApplicationController
   def destroy
     @profile.destroy
     respond_to do |format|
-      format.html { redirect_to profiles_url, notice: 'Profile was successfully destroyed.' }
+      format.html { redirect_to campaign_profiles_path(@campaign), notice: 'Profile was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
